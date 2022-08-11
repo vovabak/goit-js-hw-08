ccfb44c54f9341bfe905e93060760c63fa32db23
Volodymyr Bakovskyi <vovabak84@gmail.com>
2022-08-11 16:11:29 +0300

    task 1, 2 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+const throttle = require("lodash.throttle");
 
+const iframe = document.querySelector("iframe");
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const currentTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+const player = new Player(iframe);
+
+if (currentTime && currentTime.seconds < currentTime.duration) {
+    player.setCurrentTime(currentTime.seconds);
+}
+
+player.on("timeupdate", throttle(setCurrentTime, 1000));
+
+function setCurrentTime(data) {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+}
