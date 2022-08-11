1043a963cd6713edf10b067dad5503deebba1fee
Volodymyr Bakovskyi <vovabak84@gmail.com>
2022-08-11 18:00:25 +0300

    task 3 completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+const throttle = require("lodash.throttle");
 
+const formRef = document.querySelector('.feedback-form');
+const emailInputRef = document.querySelector('input');
+const messageRef = document.querySelector('textarea');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const localDataSaved = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+    
+formRef.addEventListener('submit', onSubmitClick);
+formRef.addEventListener('input', throttle(onFormFieldsTyping, 500));
+
+function onFormFieldsTyping() {
+    
+    const localData = {
+        email: emailInputRef.value,
+        message: messageRef.value,
+    }
+
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(localData));
+}
+
+if (localDataSaved) {
+    emailInputRef.value = localDataSaved.email;
+    messageRef.value = localDataSaved.message;
+}
+
+function onSubmitClick(event) {
+    event.preventDefault();
+
+    console.log(`email: ${emailInputRef.value}, message: ${messageRef.value}`);
+    
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+
+    event.currentTarget.reset();
+};
