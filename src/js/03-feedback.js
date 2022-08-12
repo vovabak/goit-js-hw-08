bb6ce2591a37b31c3693906eded43ed4d4de1861
Volodymyr Bakovskyi <vovabak84@gmail.com>
2022-08-12 16:38:17 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,39 @@
 const throttle = require("lodash.throttle");
 
 const formRef = document.querySelector('.feedback-form');
-const emailInputRef = document.querySelector('input');
-const messageRef = document.querySelector('textarea');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 const localDataSaved = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
+let formData = localDataSaved ?? {};
+
+if (formData.email) formRef.elements.email.value = formData.email;
+if (formData.message) formRef.elements.message.value = formData.message;
+
+// formRef.elements.email.value = formData?.email;
+// formRef.elements.message.value = formData?.message;
     
 formRef.addEventListener('submit', onSubmitClick);
 formRef.addEventListener('input', throttle(onFormFieldsTyping, 500));
 
-function onFormFieldsTyping() {
-    
-    const localData = {
-        email: emailInputRef.value,
-        message: messageRef.value,
-    }
+function onFormFieldsTyping(event) {
 
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(localData));
-}
+    formData[event.target.name] = event.target.value;
 
-if (localDataSaved) {
-    emailInputRef.value = localDataSaved.email;
-    messageRef.value = localDataSaved.message;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));    
 }
 
 function onSubmitClick(event) {
     event.preventDefault();
 
-    console.log(`email: ${emailInputRef.value}, message: ${messageRef.value}`);
-    
+    if (event.currentTarget.elements.email.value === '' || event.currentTarget.elements.message.value === '') {
+        alert("Необхідно заповнити усі поля");
+        return;
+    }
+
+    console.log(formData);    
+
+    formData = {};
+        
     localStorage.removeItem(LOCALSTORAGE_KEY);
 
     event.currentTarget.reset();
